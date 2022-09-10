1fb3675ac3254ece2c1684a66c5c9e95dc1205df
Mohamed Amine Ouertani <99047790+MedAmineOuertani@users.noreply.github.com>
2022-09-10 15:12:41 -0400

    Update dashboardController.js

--- controller/dashboardController.js
+++ controller/dashboardController.js
@@ -1,5 +1,15 @@
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const Doctor = require('../models/doctors');
+const Patient = require('../models/patients');
+const Appointment = require('../models/appointments');
+
 
 exports.getDashboard = catchAsyncErrors(async(req,res,next)=>{
-    res.render("dashboard");
+    const doctors = await Doctor.find({});
+    const patients = await Patient.find({});
+    const date = new Date(Date.now());
+    const appointments = await Appointment.find({dateOfAppointment:{$gte:date}});
+    res.render("dashboard",{
+        doctors:doctors.length,patients:patients.length,appointments:appointments.length
+    });
 })
